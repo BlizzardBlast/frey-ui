4d4b85a2e5d6329db2a815a2b0574ce0043bf415
BlizzardBlast <fdsurya7@gmail.com>
2025-02-08 08:31:03 +0700

    fix: fixed chip

--- apps/storybook/src/stories/Chip/Chip.stories.tsx
+++ apps/storybook/src/stories/Chip/Chip.stories.tsx
@@ -7,6 +7,53 @@ const meta: Meta<typeof Chip> = {
   component: Chip,
   parameters: {
     layout: 'centered'
+  },
+  argTypes: {
+    label: {
+      control: {
+        type: 'text'
+      },
+      description: 'The text to display in the chip'
+    },
+    onClick: {
+      type: 'function',
+      description: 'The event handler for the click event',
+      action: 'clicked'
+    },
+    variant: {
+      description: 'The variant of the chip',
+      table: {
+        type: {
+          summary: 'default | outlined'
+        }
+      },
+      control: {
+        type: 'select'
+      },
+      options: ['default', 'outlined']
+    },
+    style: {
+      description: 'The style of the chip',
+      control: {
+        type: 'object'
+      },
+      table: {
+        type: {
+          summary: 'CSSProperties'
+        }
+      }
+    },
+    className: {
+      description: 'The class name of the chip',
+      control: {
+        type: 'text'
+      },
+      table: {
+        type: {
+          summary: 'string'
+        }
+      }
+    }
   }
 } satisfies Meta<typeof Chip>;
 
@@ -15,13 +62,40 @@ type Story = StoryObj<typeof Chip>;
 
 export const Non_Clickable_Chip: Story = {
   args: {
-    label: 'Chip'
+    label: 'Chip',
+    onClick: undefined,
+    variant: 'default',
+    className: undefined,
+    style: undefined
   }
 } satisfies Story;
 
 export const Clickable_Chip: Story = {
   args: {
     label: 'Chip',
-    onClick: fn()
+    onClick: fn(),
+    variant: 'default',
+    className: undefined,
+    style: undefined
+  }
+} satisfies Story;
+
+export const Outlined_Non_Clickable_Chip: Story = {
+  args: {
+    label: 'Chip',
+    onClick: undefined,
+    variant: 'outlined',
+    className: undefined,
+    style: undefined
+  }
+} satisfies Story;
+
+export const Outlined_Clickable_Chip: Story = {
+  args: {
+    label: 'Chip',
+    onClick: fn(),
+    variant: 'outlined',
+    className: undefined,
+    style: undefined
   }
 } satisfies Story;
--- src/Chip/index.tsx
+++ src/Chip/index.tsx
@@ -1,20 +1,46 @@
 import clsx from 'clsx';
 import styles from './Chip.module.css';
+import React from 'react';
+
+export type Variant = 'default' | 'outlined';
 
 export type ChipProps = {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
+  style?: React.CSSProperties;
+  className?: string;
+  variant?: Variant;
 };
 
-function Chip({ label, onClick }: Readonly<ChipProps>) {
+const VariantDefaultMap: Record<Variant, string> = {
+  default: styles.chip_default,
+  outlined: styles.chip_outlined
+};
+
+const VariantClickableMap: Record<Variant, string> = {
+  default: styles.chip_default_clickable,
+  outlined: styles.chip_outlined_clickable
+};
+
+function Chip({
+  label,
+  onClick,
+  style,
+  className,
+  variant = 'default'
+}: Readonly<ChipProps>) {
   return (
     <button
-      className={clsx({
-        [styles.chip]: !onClick,
-        [styles.chip_clickable]: onClick
-      })}
+      className={clsx(
+        VariantDefaultMap[variant],
+        {
+          [VariantClickableMap[variant]]: onClick
+        },
+        className
+      )}
       onClick={onClick}
       type='button'
+      style={style}
     >
       <span className={styles.chip_text}>{label}</span>
     </button>
