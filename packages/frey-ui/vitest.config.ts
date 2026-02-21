4645e9ce4972e9c2bf1e646743e1cfd4c0ee185c
BlizzardBlast <fdsurya7@gmail.com>
2026-02-21 13:54:10 +0700

    feat: fixed unit test

--- packages/frey-ui/vitest.config.ts
+++ packages/frey-ui/vitest.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    name: 'frey-ui',
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
-    include: ['src/**/*.test.ts', 'src/**/*.test.tsx']
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+    coverage: {
+      exclude: [
+        'src/**/*.module.css',
+        ...(configDefaults.coverage.exclude || [])
+      ]
+    }
   }
 });
